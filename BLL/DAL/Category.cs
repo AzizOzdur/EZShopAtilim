914bc505a7e67164e72d70107086569a5276ce74
cenk özdur <cenk zdur@LAPTOP-C1C2B5GG>
2024-10-08 17:20:16 +0300

    Add project files.

---
+++ BLL/DAL/Category.cs
@@ -0,0 +1,12 @@
+﻿#nullable disable
+namespace BLL.DAL
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Id { get; set; }
+        public List<Product> Products { get; set; } // 1 to '''''many''''''
+
+    }
+}
---
+++ BLL/DAL/Product.cs
@@ -0,0 +1,30 @@
+﻿#nullable disable // referance type'larda "?" işaretini kullanmamak için
+namespace BLL.DAL
+{
+    public class Product
+    {
+        // JAVA way:
+        //private int id;
+        //public void SetId(int id)
+        //{
+        //    this.id = id;
+        //}
+
+
+        //public int GetId()
+        //{
+        //    return this.id;
+        //}
+        
+        //C# way
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int? StockAmount { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public int CategoryId { get; set; } // foreign key için """1""" to many
+        public Category Category { get; set; }
+
+        // struct value tiptir, class referans tiptir önemli not aklında bulunsun
+    }e
+}
